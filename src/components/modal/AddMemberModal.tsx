53a5a05e4a9b817dae3bfba027b1d98aa3d0e907
Hyein <108849398+Joohyein@users.noreply.github.com>
2023-03-15 00:34:17 +0900

    Feat : invite member modal GJ-39

--- src/components/modal/AddMemberModal.tsx
+++ src/components/modal/AddMemberModal.tsx
@@ -1,24 +1,23 @@
 import styled from "styled-components";
 import Close from "../asset/icons/Close";
-import { useState } from "react";
 
 function AddMemberModal({modalRef, setInviteModal}:{modalRef:React.MutableRefObject<any>, setInviteModal:(v:boolean)=>void}) { 
     
-  return <StBackgroundContainer>
+  return (
     <StModalContainer ref={modalRef}>
         <StTitle>
             <h3>멤버 초대하기</h3>
             <Close size="24" fill="#363636" onClick={()=>setInviteModal(false)} cursor="pointer"/>
         </StTitle>
         <StInputContainer>
-
+            <StInputBox type="email" />
+            <StInviteBtn>초대 보내기</StInviteBtn>
         </StInputContainer>
 
-        <StSub>초대 중인 멤버</StSub>
+        <StSub>초대 중인 사람</StSub>
 
         <StInviting>
             <StMember>
-            <StUserProfile></StUserProfile>
             <StUser>
                 <h3>김코코</h3>
                 <h5>coco@gmail.com</h5>
@@ -27,44 +26,40 @@ function AddMemberModal({modalRef, setInviteModal}:{modalRef:React.MutableRefObj
             <StCancelBtn>초대 취소</StCancelBtn>
         </StInviting>
     </StModalContainer>
-</StBackgroundContainer>
+  )
 }
 
 export default AddMemberModal;
 
-const StBackgroundContainer = styled.div`
-    width: 100%;
-    overflow: hidden;
-    position: fixed;
-    background-color: rgba(0,0,0,0.25);
-`;
 const StModalContainer = styled.div`
-  width: 500px;
-  height: 500px;
-  z-index: 999;
-  position: fixed;
-  top: 50%;
-  left: 50%;
-  transform: translate(-50%, -50%);
-  background-color: white;
+    display: flex;
+    flex-direction: column;
+    gap: 24px;
+    width: 512px;
+    z-index: 999;
+    position: fixed;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    background-color: white;
 
-  border: 3px solid gray;
-  padding: 20px;
-  box-sizing: border-box;
-  overflow: hidden;
+    border: 3px solid gray;
+    padding: 20px;
+    box-sizing: border-box;
+    overflow: hidden;
 `;
 const StTitle = styled.div`
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
 
-  h3 {
+    h3 {
 
-  }
-  div {
+    }
+    div {
     font-size: 32px;
     cursor: pointer;
-  }
+    }
 `;
 
 const StSub = styled.h3`
@@ -73,7 +68,18 @@ const StSub = styled.h3`
   color: gray;
 `;
 const StInputContainer = styled.div`
-  
+  display: flex;
+  justify-content: space-between;
+  gap: 24px;
+`;
+const StInputBox = styled.input`
+    width: 70%;
+    height: 24px;
+`;
+const StInviteBtn = styled.button`
+    width: 30%;
+    border: none;
+    cursor: pointer;
 `;
 
 const StInviting = styled.div`
@@ -84,13 +90,6 @@ const StInviting = styled.div`
 const StMember = styled.div`
   display: flex;
   gap: 12px;
-`;
-const StUserProfile = styled.div`
-  background-color: whitesmoke;
-  box-shadow: 0px 0px 5px 0px lightgray;
-  border-radius: 8px;
-  width: 32px;
-  height: 32px;
 `;
 const StUser = styled.div`
   display: flex;
