14d3d77c8b7f0cb7259398fca25777a6ce0fd253
Ryum <ryumforart@gmail.com>
2023-03-18 01:58:15 +0900

    Refactor : unset husky

--- src/api/auth.ts
+++ src/api/auth.ts
@@ -1,12 +1,6 @@
 import instance from "./instance/instance";
 
-const login = async ({
-  email,
-  password,
-}: {
-  email: string;
-  password: string;
-}) => {
+const login = async ({email, password} : {email: string, password: string}) => {
   const response = instance.post("/api/users/login", { email, password });
   return response;
 };
