4b6a11c0dcfec4d28a6166461fc91e8b1d1d9b3e
Ryum <ryumforart@gmail.com>
2023-03-18 16:59:02 +0900

    Feat : When logged in redirect to select-workspace GJ-155

--- src/pages/CelebrateSignUp.tsx
+++ src/pages/CelebrateSignUp.tsx
@@ -1,6 +1,17 @@
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 import Wrapper from "../components/common/Wrapper";
+import useIsLogin from "../hooks/useIsLogin";
 
 const CelebrateSignUp = () => {
+
+  const isLogin = useIsLogin();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if(isLogin === true) return navigate('/select-workspace');
+  }, [isLogin])
+
   return (
     <Wrapper>
       CelebrateSignUp
