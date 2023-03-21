f645aecc6aa03ace83be45e09ba4fc6056ddfbca
Hyein <108849398+Joohyein@users.noreply.github.com>
2023-03-21 14:33:21 +0900

    select workspace ing

--- src/pages/SelectWorkspace.tsx
+++ src/pages/SelectWorkspace.tsx
@@ -1,5 +1,15 @@
+import { useQuery } from "react-query";
+import { getWorkspaceList } from "../api/selectWorkspace";
+import Wrapper from "../components/common/Wrapper";
+import MyWorkspaceList from "../components/selectWorkspace/MyWorkspaceList";
+
 const SelectWorkspace = () => {
-  return <div>SelectWorkspace</div>;
+  const { data } = useQuery('workspaceList', getWorkspaceList);
+
+  return <Wrapper>
+    <MyWorkspaceList workspaceList={data} />
+  </Wrapper>
 };
 
 export default SelectWorkspace;
+
