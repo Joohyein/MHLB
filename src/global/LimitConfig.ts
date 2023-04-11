862244d141173395c7cd7edff1672a8cd6a9f699
Joohyein <h2ini@naver.com>
2023-04-11 17:53:24 +0900

    Feat: length limit GJ-333

---
+++ src/global/LimitConfig.ts
@@ -0,0 +1,5 @@
+export const GvWorkspaceNameLength = 50;
+export const GvWorkspaceDescLength = 200;
+export const GvUserNameLength = 12;
+export const GvUserJobLength = 40;
+export const GvUserStatusMessageLength = 60;
