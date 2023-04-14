85ac3758cc53678a8181e1186a2b476be7042d9a
RyumForCode <ryumforart@gmail.com>
2023-04-14 19:32:06 +0900

    Feat : Nav bar Route GJ-377

--- src/pages/Landing.tsx
+++ src/pages/Landing.tsx
@@ -1,4 +1,3 @@
-import NavBarLanding from "../components/common/NavBarLanding";
 import Wrapper from "../components/common/Wrapper";
 import Intro from "../components/landing/Intro";
 import Slider from "../components/landing/slider/Slider";
@@ -6,7 +5,6 @@ import Slider from "../components/landing/slider/Slider";
 const Landing = () => {
   return (
     <Wrapper>
-      <NavBarLanding />
       <Intro />
       <Slider />
     </Wrapper>
